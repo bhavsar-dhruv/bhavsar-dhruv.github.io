ee9e0afc61285795479bb44c07674dfea1fe1f26
bhavsar-dhruv <bhavsardhruvn12@gmail.com>
2026-03-04 00:49:54 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',   // important for GitHub Pages
   plugins: [
     tailwindcss(),
     react(),
